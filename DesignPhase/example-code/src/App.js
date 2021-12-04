cf4203159e46faa0443c76cf8478558ac77efb0f
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-12-04 11:56:30 +0100

    added example docstring, state, method, prop

--- DesignPhase/example-code/src/App.js
+++ DesignPhase/example-code/src/App.js
@@ -1,6 +1,21 @@
-export default function App() {
+import React from 'react'
+
+/**
+ * empty description
+ * the props are just an example
+ * they make no sense for the
+ * top level component
+ */
+export default function App(props) {
+
+  const [exampleState, setExampleState] = React.useState(""); 
+
+  const foo = () => {
+    console.log("bar")
+  }
+
   return (
-    <div className="App">
+    <div className="App" id={props.id}>
       Add components here
     </div>
   );
