6928cad1c0b76cd5bca99aafe158067df6e377db
uxkkp <jana.zeller@student.kit.edu>
2021-12-19 17:49:58 +0100

    added a cookie consent modal

---
+++ DesignPhase/example-code/src/components/CookieConsentModal/CookieConsentModal.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+/**
+ * displays an error message
+ * @param {Object} props 
+ *  props.open -> whether the error message modal should be displayed
+ *  props.onClose -> handles closing of the modal
+ *  props.error -> error function
+ * @returns a jsx file containing a modal with the given error message
+ */
+function CookieConsentModal(props) {
+    
+    let i = props.open;
+    i = props.onClose;
+    i = props.message;
+    
+
+    return <div>
+    </div>;
+}
+
+export default ErrorMessageModal;
