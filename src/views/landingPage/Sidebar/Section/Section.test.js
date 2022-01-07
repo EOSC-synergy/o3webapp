77a47a1b4bf1410e1cddc3652f0f6217a17ef838
Jana-Z <github3@jana-zeller.de>
2022-01-07 17:40:28 +0100

    added tests

---
+++ src/views/landingPage/Sidebar/Section/Section.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Section from './Section';
+import renderer from 'react-test-renderer';
+import {render, screen} from '@testing-library/react';
+import defaultStructure from '../../../../config/defaultConfig.json';
+import "@testing-library/jest-dom/extend-expect";
+
+it('Section renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Section />, div);
+});
+
+// report error if no components are entered
+it('Section reports error if no components are passed to render', () => {
+    const reportError = jest.fn();
+    const name = "1";
+    const div = document.createElement('div');
+    ReactDOM.render(<Section reportError={reportError} name={name} />, div);
+    expect(reportError).toHaveBeenCalledWith(`Section ${name} was provided with no components`)
+})
+
+// report error if invalid components are entered
+it('Section reports error if invalid component is entered', () => {
+    const reportError = jest.fn();
+    const name = "1";
+    const compName = "blob";
+    const div = document.createElement('div');
+    ReactDOM.render(<Section reportError={reportError} components={[compName]} name={name} />, div);
+    expect(reportError).toHaveBeenCalledWith(`Section ${name} found no match for an input component ${compName}`)
+})
+
+// renders props.name
+it('Section renders the given name', () => {
+    const name = "Test";
+    render(<Section components={["LatitudeBandSelector"]} name={name} />);
+    expect(screen.getByTestId('section')).toHaveTextContent(name);
+})
+
+
+// Snapshot test
+it('Section renders correctly from config file', () => {
+    for (let section in defaultStructure.sections) {
+        const tree = renderer
+            .create(<Section name={section.name} reportError={() => {}} components={section.components} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    }
+});
---
+++ src/views/landingPage/Sidebar/Sidebar.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Sidebar from './Sidebar';
+import renderer from 'react-test-renderer';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+
+
+it('Sidebar renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Sidebar />, div);
+});
+
+// Snapshot test
+it('Sidebar renders correctly', () => {
+    const treeClosed = renderer
+        .create(<Sidebar isOpen={false} />)
+        .toJSON();
+    expect(treeClosed).toMatchSnapshot();
+
+    const div = document.createElement('div');
+    ReactDOM.render(<Sidebar isOpen={true} />, div);
+    expect(div).toMatchSnapshot();
+});
+
+// expect there to be a download button
+it('Sidebar has a download button', () => {
+    render(<Sidebar />)
+    const download = screen.getByText(/Download/i);
+    expect(download).not.toBeDisabled();
+    userEvent.click(download);
+    // test for when download button is clicked
+    // TODO
+})
