d32ee3e784d8e7960c1811c14619c3460a8090c9
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-07 14:44:16 +0100

    static octs preview to work with instead of silly picture

--- src/views/landingPage/Graph/Graph.js
+++ src/views/landingPage/Graph/Graph.js
@@ -1,10 +1,29 @@
 import * as React from 'react';
-import {useSelector} from 'react-redux'
-import { useGetRawPlotDataMutation } from '../../../services/API/apiSlice';
-import { selectCurrentModelGroups } from '../../../store/modelsSlice';
-import { selectCurrentPlotId, selectCurrentPlotType, selectCurrentSettings } from '../../../store/plotSlice';
-import { selectCurrentReferenceSettings } from '../../../store/referenceSlice';
-import {calculatePlotSeries} from "../../../services/math/math";
+import Chart from "react-apexcharts"
+import data from "./default-data.json"
+import settings from "./default-settings.json"
+import { convertToStrokeStyle, colourNameToHex } from "../../../utils/optionsFormatter"
+
+// generate static preview for octs plot
+const yearsOnXAxis = [...Array(141).keys()].map(value => `${value + 1960}`)
+const ySeries = data.map(modelObj => {        
+        return {
+            name: modelObj.model,
+            data: modelObj.y.map(number => number.toFixed(2)),
+        }
+    }
+)
+const colors = data.map(modelObj => colourNameToHex(modelObj.plotstyle.color))
+const strokes = data.map(modelObj => convertToStrokeStyle(modelObj.plotstyle.linestyle))
+const lineWidth = data.map(el => 2) // default is 2
+
+console.log(settings)
+settings.options.xaxis.categories.push(...yearsOnXAxis)
+settings.options.colors.push(...colors)
+settings.options.stroke.dashArray.push(...strokes)
+settings.options.stroke.width.push(...lineWidth)
+settings.series.push(...ySeries)
+console.log(settings)
 
 /**
  * A containter for the displayed graph.
@@ -12,35 +31,18 @@ import {calculatePlotSeries} from "../../../services/math/math";
  */
 function Graph(props) {
 
-    // const plotType = selectCurrentPlotType()
-    // const plotId = selectCurrentPlotId()
-    // const options = useSelector(state => selectCurrentSettings(state, plotId))
-    // const models = useSelector(state => selectCurrentModelGroup(state, plotId, "all"))
-    // const referenceSettings = useSelector(state => selectCurrentReferenceSettings(state, plotId))
     
-    // const { data, isError, error, isLoading, isSuccess } = useGetRawPlotDataMutation(
-    //     {
-    //         plotType,
-    //         latMin: options.latMin,
-    //         latMax: options.latMax,
-    //         modelList,
-    //     }
-    // )
-    
-    // if (isSuccess) {
-    //     // updateCurrentModelGroup with fetched data
-    // }
 
-
-    // // display loading spinner instead of graph until isSuccess = true
-    // // then load data
-    
-    // const seriesData = generatePlotSeries(models)
-
-    return (<div>
-        Graph
-        {/* Graph displayed with ApexCharts goes here */}
-    </div>);
+    return (<>
+        {/* for the OCTS Plot */}
+        <Chart
+                options={settings.options}
+                series={settings.series}
+                type={"line"}
+                height={"600px"}
+            />
+        
+    </>);
 }
 
 export default Graph;
