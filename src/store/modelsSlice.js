5ab681cfc5b3706d3dbce121ec86cd01eecf7c1a
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-09 11:47:28 +0100

    created initial state for modelslice, the state contains a lookup table for each plot id (currently tco3_zm, tco3_return)

--- src/store/modelsSlice.js
+++ src/store/modelsSlice.js
@@ -1,9 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    "octs": { // plot name (id)
-        "all": { // model group
-            name: "Somehting",
+    // active plot id is stored in state.plot.plotId
+    "tco3_zm": { 
+        // this objects holds key-value-pairs, the keys being the model-group 
+        // identifier and the values being the settings for each group 
+        "all": { 
+            // model group storing all information until it is possible 
+            // to implement more model groups
+            name: "All OCTS models",
             modelList: ["CCMI-1_ACCESS_ACCESS-CCM-refC2"],
             models: { // models is lookup table
                 "CCMI-1_ACCESS_ACCESS-CCM-refC2": { // single model
@@ -12,6 +17,34 @@ const initialState = {
                         x: [42],
                         y: [42],
                     },
+                    color: "#000000",
+                    plotStyle: "solid",
+                    hidden: false, // show/hide individual models from a group
+                    mean: true,
+                    derivative: true,
+                    median: true,
+                    percentile: true,
+                }
+            },
+            hidden: false, // show/hide complete group
+            derivativeVisible: false,
+            meanVisible: false,
+            medianVisible: false,
+            percentileVisible: false
+        }
+    },
+    "tco3_return": {
+        "all": { 
+            name: "All Return/Recovery models",
+            modelList: ["CCMI-1_ACCESS_ACCESS-CCM-refC2"],
+            models: { // models is lookup table
+                "CCMI-1_ACCESS_ACCESS-CCM-refC2": { // single model
+                    institute: "IMK",
+                    dataset: {
+                        x: [42],
+                        y: [42],
+                    },
+                    hidden: false, // show/hide individual models from a group
                     mean: true,
                     derivative: true,
                     median: true,
@@ -20,7 +53,7 @@ const initialState = {
                     plotStyle: "solid",
                 }
             },
-            hidden: false,
+            hidden: false, // show/hide complete group
             derivativeVisible: false,
             meanVisible: false,
             medianVisible: false,
