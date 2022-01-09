1de8d154526a58997f5a47a89157220e331ab79d
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-09 12:10:24 +0100

    added set reducer for statistical values and visibility, errors get thrown if an internal error occurs

--- src/store/modelsSlice.js
+++ src/store/modelsSlice.js
@@ -1,63 +1,66 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    // active plot id is stored in state.plot.plotId
-    "tco3_zm": { 
-        // this objects holds key-value-pairs, the keys being the model-group 
-        // identifier and the values being the settings for each group 
-        "all": { 
-            // model group storing all information until it is possible 
-            // to implement more model groups
-            name: "All OCTS models",
-            modelList: ["CCMI-1_ACCESS_ACCESS-CCM-refC2"],
-            models: { // models is lookup table
-                "CCMI-1_ACCESS_ACCESS-CCM-refC2": { // single model
-                    institute: "IMK",
-                    dataset: {
-                        x: [42],
-                        y: [42],
-                    },
-                    color: "#000000",
-                    plotStyle: "solid",
-                    hidden: false, // show/hide individual models from a group
-                    mean: true,
-                    derivative: true,
-                    median: true,
-                    percentile: true,
-                }
-            },
-            hidden: false, // show/hide complete group
-            derivativeVisible: false,
-            meanVisible: false,
-            medianVisible: false,
-            percentileVisible: false
-        }
-    },
-    "tco3_return": {
-        "all": { 
-            name: "All Return/Recovery models",
-            modelList: ["CCMI-1_ACCESS_ACCESS-CCM-refC2"],
-            models: { // models is lookup table
-                "CCMI-1_ACCESS_ACCESS-CCM-refC2": { // single model
-                    institute: "IMK",
-                    dataset: {
-                        x: [42],
-                        y: [42],
-                    },
-                    hidden: false, // show/hide individual models from a group
-                    mean: true,
-                    derivative: true,
-                    median: true,
-                    percentile: true,
-                    color: "#000000",
-                    plotStyle: "solid",
-                }
-            },
-            hidden: false, // show/hide complete group
-            derivativeVisible: false,
-            meanVisible: false,
-            medianVisible: false,
-            percentileVisible: false
+    // currently active plot
+    plotId: "tco3_zm",
+    settings: {
+        "tco3_zm": { 
+            // this objects holds key-value-pairs, the keys being the model-group 
+            // identifier and the values being the settings for each group 
+            "all": { 
+                // model group storing all information until it is possible 
+                // to implement more model groups
+                name: "All OCTS models",
+                modelList: ["CCMI-1_ACCESS_ACCESS-CCM-refC2"],
+                models: { // models is lookup table
+                    "CCMI-1_ACCESS_ACCESS-CCM-refC2": { // single model
+                        institute: "IMK",
+                        dataset: {
+                            x: [42],
+                            y: [42],
+                        },
+                        color: "#000000",
+                        plotStyle: "solid",
+                        isVisible: true, // show/hide individual models from a group
+                        mean: true,
+                        derivative: true,
+                        median: true,
+                        percentile: true,
+                    }
+                },
+                hidden: false, // show/hide complete group
+                derivativeVisible: false,
+                meanVisible: false,
+                medianVisible: false,
+                percentileVisible: false
+            }
+        },
+        "tco3_return": {
+            "all": { 
+                name: "All Return/Recovery models",
+                modelList: ["CCMI-1_ACCESS_ACCESS-CCM-refC2"],
+                models: { // models is lookup table
+                    "CCMI-1_ACCESS_ACCESS-CCM-refC2": { // single model
+                        institute: "IMK",
+                        dataset: {
+                            x: [42],
+                            y: [42],
+                        },
+                        hidden: false, // show/hide individual models from a group
+                        mean: true,
+                        derivative: true,
+                        median: true,
+                        percentile: true,
+                        color: "#000000",
+                        plotStyle: "solid",
+                    }
+                },
+                hidden: false, // show/hide complete group
+                derivativeVisible: false,
+                meanVisible: false,
+                medianVisible: false,
+                percentileVisible: false
+            }
         }
     }
 }
@@ -66,10 +69,34 @@ const modelsSlice = createSlice({
     name: "models",
     initialState,
     reducers: {
+        // set models
+        // add models
+
         updatedModelGroup(state, action) { },
         addedModelGroup(state, action) { },
-        setVisibility(state, action) { },
-        setStatisticalValuesIncluded(state, action) { },
+        setVisibility(state, action) { 
+            const { groupID, modelID, isVisible } = action.payload
+            const activeSettings = state.settings[state.plotId]
+            const activeModel = activeSettings[groupID].models[modelID]
+            if (activeModel === undefined) {
+                throw `tried to access model with modelID "${modelID}" that is not yet defined!`
+            } 
+            activeModel.isVisible = isVisible
+        },
+        setStatisticalValuesIncluded(state, action) { 
+            // svType has to be a string of the form: 
+            // mean | derivative | median | percentile
+            const { groupID, modelID, svType, isIncluded } = action.payload;
+            const activeSettings = state.settings[state.plotId];
+            const activeModel = activeSettings[groupID].models[modelID]
+            if (activeModel === undefined) {
+                throw `tried to access model with modelID "${modelID}" that is not yet defined!`
+            }
+            if (activeModel[svType] === undefined) { // svType doesn't represent a valid statistical value
+                throw `tried to set statistial value "${svType}" that is not a valid statistical value`
+            }
+            activeModel[svType] = isIncluded
+        },
         addStatisticalValue(state, action) { },
         removeStatisticalValue(state, action) { },
     }
