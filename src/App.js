5590d2a86dc10ee34e7373b60c98f8240c49ecfb
Jana-Z <github3@jana-zeller.de>
2022-01-10 15:43:23 +0100

    added comments

--- src/App.js
+++ src/App.js
@@ -16,16 +16,28 @@ function App() {
     const [errorMessage, setErrorMessage] = React.useState(null);  // if errorMessage null no error
     const [isCookieConsentModalVisible, setCookieConsentModalVisibility] = React.useState(true);
 
+    /**
+     * function to report an error from other components
+     * automatically opens errorModal
+     * @param {string} msg the message of the reported error
+     */
     const reportError = (msg) => {
         setErrorModalVisible(true);
         setErrorMessage(msg);
     }
     
+    /**
+     * closes the error modal
+     */
     const closeErrorModal = () => {
         setErrorModalVisible(false);
     }
 
+    /**
+     * closes the cookie consent modal
+     */
     const onCloseCookieConsentModal = () => {
+        setCookieConsentModalVisibility(false);
     }
     
 
