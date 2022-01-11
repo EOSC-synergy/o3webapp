766b3673619d8e3fb37135a80639e73c6e282fef
Nikolai Prjanikov <ufjxp@student.kit.edu>
2022-01-11 18:41:34 +0100

    Partially implemented the funtionality of the PlotNameField component

--- src/views/landingPage/Sidebar/InputComponents/PlotNameField/PlotNameField.js
+++ src/views/landingPage/Sidebar/InputComponents/PlotNameField/PlotNameField.js
@@ -18,11 +18,22 @@ function PlotNameField(props) {
     // The label displayed inside the TextField while nothing is typed in
     const textFieldLabel = "New Plot Name";
 
+     
+    // Dispatcher to dispatch the plot name change action
+    const dispatch = useDispatch();
+
+    // Handles the change if the text in TextField is modified
+    const handleChange = (event) => {
+        // TODO
+        // dispatch(setTitle({title: event.target.value}));
+    }
+    
+
     return ( <>
         <Divider><Typography>{componentTitle}</Typography></Divider>
         <Box sx={{paddingLeft: '8%', paddingRight: '8%', paddingTop: '3%', paddingBottom: '3%'}}>
             <FormControl sx={{width: '100%' }}>
-                <TextField id="standard-basic" label={textFieldLabel} variant="standard" />
+                <TextField id="standard-basic" label={textFieldLabel} variant="standard" onChange={handleChange} />
             </FormControl>
         </Box>
         </>
