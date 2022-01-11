0a55b26cf190bf2877e78682fe6759536f104092
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-11 21:56:46 +0100

    wrote documentation for the static graph placeholder

--- src/views/landingPage/Graph/Graph.js
+++ src/views/landingPage/Graph/Graph.js
@@ -4,8 +4,15 @@ import data from "./default-data.json"
 import settings from "./default-settings.json"
 import { convertToStrokeStyle, colourNameToHex } from "../../../utils/optionsFormatter"
 
-// generate static preview for octs plot
+/**
+ * Static generation of the years on the x-axis, this will be fetched from the
+ * api later
+ */
 const yearsOnXAxis = [...Array(141).keys()].map(value => `${value + 1960}`)
+/**
+ * Transforming the data from default-data.json, this data will be fetched 
+ * from the api later
+ */
 const ySeries = data.map(modelObj => {        
         return {
             name: modelObj.model,
@@ -13,10 +20,20 @@ const ySeries = data.map(modelObj => {
         }
     }
 )
+/**
+ * Static color mapping to provide color in hexformat for apexcharts
+ */
 const colors = data.map(modelObj => colourNameToHex(modelObj.plotstyle.color))
+/**
+ * Static humand readable stroke style mapping to apexchart stroke style
+ */
 const strokes = data.map(modelObj => convertToStrokeStyle(modelObj.plotstyle.linestyle))
+/**
+ * Static humand readable linewidth mapping to apexchart linewidth
+ */
 const lineWidth = data.map(el => 2) // default is 2
 
+// build the big object that is handed over to apexcharts
 settings.options.xaxis.categories.push(...yearsOnXAxis)
 settings.options.colors.push(...colors)
 settings.options.stroke.dashArray.push(...strokes)
@@ -24,12 +41,16 @@ settings.options.stroke.width.push(...lineWidth)
 settings.series.push(...ySeries)
 
 /**
- * A containter for the displayed graph.
- * Displays the currently selected plot.
+ * Currently there is no dynamic data linking. The graph will always
+ * render the data from default-data.json in this folder. This is
+ * just a preview to work with until the API is implemented and
+ * synced with redux and the UI (input components).
+ * 
+ * @param {*} props currently not used
+ * @returns a svg rendered element that represents a graph, this is done by 
+ *          the apexcharts library
  */
 function Graph(props) {
-
-    
 
     return (<>
         {/* for the OCTS Plot */}
