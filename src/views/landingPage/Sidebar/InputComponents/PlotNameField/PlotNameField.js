9b28402c3a81959187bb4e0b130dc8b09d212f53
uqgie <simon.roenn@student.kit.edu>
2022-01-13 12:36:53 +0100

    Changed normal comments to JSDoc comments

--- src/views/landingPage/Sidebar/InputComponents/PlotNameField/PlotNameField.js
+++ src/views/landingPage/Sidebar/InputComponents/PlotNameField/PlotNameField.js
@@ -12,19 +12,19 @@ import Typography from "@mui/material/Typography";
  */
 function PlotNameField(props) {
     
-    // The title that should be displayed above the TextField
+    /** The title that should be displayed above the TextField. */
     const componentTitle = "PLOT NAME";
 
-    // The label displayed inside the TextField while nothing is typed in
+    /** The label displayed inside the TextField while nothing is typed in. */
     const textFieldLabel = "New Plot Name";
 
-    // The max. length of the plot name
+    /** The max. length of the plot name */
     const PLOT_NAME_MAX_LEN = 40;
      
-    // Dispatcher to dispatch the plot name change action
+    // /** Dispatcher to dispatch the plot name change action. */
     //const dispatch = useDispatch();
 
-    // Handles the change if the text in TextField is modified
+    /** Handles the change if the text in TextField is modified. */
     const handleChange = (event) => {
         if (event.target.value.length > PLOT_NAME_MAX_LEN) {
             event.target.value = event.target.value.slice(0, PLOT_NAME_MAX_LEN);
