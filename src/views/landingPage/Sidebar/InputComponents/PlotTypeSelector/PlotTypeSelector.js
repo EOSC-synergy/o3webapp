97d92ad3b5a0bff113fc8cd4b21f5eabd6b98148
simon.vonroenn <simon.vonroenn@web.de>
2022-01-13 20:35:07 +0100

    Manually merge single files from branch 'feat_plotTypeSelector_#21' into 'develop' Feature PlotTypeSelector component (no functionality) [#21]
    See merge request !4

--- src/views/landingPage/Sidebar/InputComponents/PlotTypeSelector/PlotTypeSelector.js
+++ src/views/landingPage/Sidebar/InputComponents/PlotTypeSelector/PlotTypeSelector.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { useGetPlotsQuery } from "../../../../../services/API/apiSlice";
+// import { useGetPlotsQuery } from "../../../../../services/API/apiSlice";
+import PropTypes from "prop-types";
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+import { sleep } from '../../../../../utils/mock_dev';
+
 
 /**
  * enables the user to select a different plot type
@@ -9,21 +18,90 @@ import { useGetPlotsQuery } from "../../../../../services/API/apiSlice";
  */
 function PlotTypeSelector(props) {
 
+    // for dev reasons, should eventually be moved to the redux / API module
     /**
-     * gets all available plot types
+     * @todo move to redux eventually
+     */
+    const [plotType, setPlotType] = React.useState('');
+    /**
+     * @todo move to redux eventually
+    */
+    const [isLoading, setIsLoading] = React.useState(true);
+    /**
+     * @todo move to redux eventually
+     */
+    const [plotTypes, setPlotTypes] = React.useState([]);
+
+
+
+    /**
+     * mocks a function to get all available plot types
+     * @todo connect with redux store
      */
     const getAllAvailablePlotTypes = () => {
         // const {data, isSuccess, isLoading, isError, error} = useGetPlotsQuery()
         // re-render component based on isLoading <-> isSuccess
+
+        // for dev reasons simulating response
+        // if api call fails, an error wil be evoked by the API module -> does not need to be handled here
+        sleep(3000).then(() => {
+            setPlotTypes([
+                "tco3_zm",
+                "tco3_return"
+            ]);
+            setIsLoading(false);
+        });
+        let err = undefined;
+        if (err) {
+            props.reportError(err.message);
+        }
     }
 
-    let i = props.reportError;
+    /**
+     * mocks a call to the redux store to change the plot type
+     * @param {event} event the event that called this function
+     * @todo connect with redux store
+     */
+    const changePlotType = (event) => {
+        setPlotType(event.target.value);
+    }
 
     return (
-        <>
-            PlotTypeSelector
-        </>
+        <FormControl sx={{ width: '100%' }} data-testid="plotTypeSelectorForm">
+            {getAllAvailablePlotTypes()}
+            <InputLabel id="plotTypeLabel" data-testid="plotTypeSelector">Plot Type</InputLabel>
+            <Select
+                labelId="plotTypeLabel"
+                id="plotType"
+                value={plotType}
+                label="Plot Type"
+                onChange={changePlotType}
+            >
+                {
+                isLoading ? 
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}
+                    >
+                        <CircularProgress />
+                    </Box>
+                :
+                    plotTypes.map((name, idx) => {
+                        return (
+                            <MenuItem key={idx} value={name}>{name}</MenuItem>
+                        )
+                    })
+                }
+            </Select>
+        </FormControl>
     );
+}
+
+PlotTypeSelector.propTypes = {
+    reportError: PropTypes.func.isRequired,
 }
 
 export default PlotTypeSelector;
