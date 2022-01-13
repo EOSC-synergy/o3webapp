8dd8e863a339f4fe0e6ff1366715d64a2797de7e
Nikolai Prjanikov (Tuuxic) <58004991+Tuuxic@users.noreply.github.com>
2022-01-13 12:15:01 +0100

    Added a max. length to the input string of the PlotNameField

--- src/views/landingPage/Sidebar/InputComponents/PlotNameField/PlotNameField.js
+++ src/views/landingPage/Sidebar/InputComponents/PlotNameField/PlotNameField.js
@@ -18,12 +18,18 @@ function PlotNameField(props) {
     // The label displayed inside the TextField while nothing is typed in
     const textFieldLabel = "New Plot Name";
 
+    // The max. length of the plot name
+    const PLOT_NAME_MAX_LEN = 30;
      
     // Dispatcher to dispatch the plot name change action
     //const dispatch = useDispatch();
 
     // Handles the change if the text in TextField is modified
     const handleChange = (event) => {
+        if (event.target.value.length > PLOT_NAME_MAX_LEN) {
+            event.target.value = event.target.value.slice(0, PLOT_NAME_MAX_LEN - 1);
+        }
+        
         // TODO
         // dispatch(setTitle({title: event.target.value}));
     }
