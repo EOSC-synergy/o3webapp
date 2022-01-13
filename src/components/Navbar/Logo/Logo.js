d920eb8c4e8d4b99a1a2505029209aaa964ec906
Jana-Z <github3@jana-zeller.de>
2022-01-13 20:08:20 +0100

    added comments

--- src/components/Navbar/Logo/Logo.js
+++ src/components/Navbar/Logo/Logo.js
@@ -4,28 +4,40 @@ import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
+/**
+ * The link that provides the image logo
+ */
+const logoSrc = "https://o3as.data.kit.edu/img/logos/o3as-logo.svg";
+/**
+ * The link to the O3AS Webapp
+ */
+const O3ASLink = "#";
+
+/**
+ * Returns a logo from O3AS and the text "O3AS Webapp"
+ * @param {Object} props 
+ * @param {Object} props.display when the logo should be displayed
+ * @returns {JSX.Element} containing one Image and one Text wrapped in a Link component
+ */
 const Logo = (props) => {
-    if (!('display' in props)) {
-        return (<></>)
-    }
-    return ( <>
+    return (
         <Link
-            href="#"
+            href={O3ASLink}
             sx={{ flexGrow: 1, textDecoration: 'inherit', color: 'inherit', display: props.display, minWidth: "70%" }}
             noWrap
         >   
             <Grid container sx={{ alignItems: "center", margin: '0.5em', marginLeft: '2em'}}>
-                <img style={{height: '5em', margin: '0.5em'}} src="https://o3as.data.kit.edu/img/logos/o3as-logo.svg" />
+                <img style={{height: '5em', margin: '0.5em'}} src={logoSrc} />
                 <Typography variant="h6" noWrap>
                     O3AS Webapp
                 </Typography>
             </Grid>
         </Link>
-    </>);
+    );
 };
 
 Logo.propTypes = {
-    display: PropTypes.object.isRequired
+    display: PropTypes.object
 }
 
 export default Logo;
