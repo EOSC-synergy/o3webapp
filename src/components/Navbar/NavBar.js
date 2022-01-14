682483d344efaa1cac16880e95c45a9e5fe18138
Jana-Z <github3@jana-zeller.de>
2022-01-14 21:42:38 +0100

    fixed styling

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -9,6 +9,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import Logo from './Logo/Logo';
+import Container from '@mui/material/Container';
 
 /**
  * The pages and corresponding links that should be visible in the menu
@@ -51,69 +52,73 @@ function Navbar(props) {
     };
 
     return (
-        <AppBar position="fixed" sx={{bgcolor: indigo[500]}} data-testid="Navbar">
-            <Toolbar disableGutters data-testid="Toolbar_Navbar">
-                {/* EXPANDED */}
-                <Logo display={displayExpanded} data-testid="navbar-logo-expanded" />
-                 <Box sx={{ flexGrow: 1, display: displayExpanded }} data-testid="navbar-menu-expanded" >
-                    {pages.map((page, idx) => (
-                        <Button
-                            key={idx}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                            data-testid={`navbar-expanded-menu-option-${page.label}`}
-                        >
-                            <a href={page.href} style={{textDecoration: 'inherit', color: 'inherit'}}>{page.label}</a>
-                        </Button>
-                    ))}
-                </Box>
+        // <Box sx={{ flexGrow: 1, width:"100vw" }}>
+            <AppBar position="static" sx={{ bgcolor: indigo[500], margin: 0 }} data-testid="Navbar">
+                <Container sx={{ margin:0 }}>
+                    <Toolbar disableGutters data-testid="Toolbar_Navbar">
+                        {/* EXPANDED */}
+                        <Logo display={displayExpanded} data-testid="navbar-logo-expanded" />
+                        <Box sx={{ flexGrow: 1, display: displayExpanded }} data-testid="navbar-menu-expanded" >
+                            {pages.map((page, idx) => (
+                                <Button
+                                    key={idx}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                    data-testid={`navbar-expanded-menu-option-${page.label}`}
+                                >
+                                    <a href={page.href} style={{textDecoration: 'inherit', color: 'inherit'}}>{page.label}</a>
+                                </Button>
+                            ))}
+                        </Box>
 
-                {/* COLLAPSED */}
-                <Box sx={{ flexGrow: 1, display: displayCollapsed }}>
-                    <IconButton
-                        size="large"
-                        aria-label="menu"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleOpenNavMenu}
-                        color="inherit"
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorElNav}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        open={Boolean(anchorElNav)}
-                        onClose={handleCloseNavMenu}
-                        sx={{
-                            display: { xs: 'block', md: 'none' },
-                        }}
-                        data-testid="navbar-menu-collapsed"
-                    >
-                    {pages.map((page, idx) => (
-                        <MenuItem
-                            key={idx}
-                            sx={{ my: 2, display: 'block' }}
-                            data-testid={`navbar-collapsed-menu-option-${page.label}`}
-                        >
-                            <a href={page.href} style={{textDecoration: 'inherit', color: 'inherit'}}>{page.label}</a>
-                        </MenuItem>
-                    ))}
-                    </Menu>
-                </Box>
-                <Logo display={displayCollapsed} data-testid="navbar-logo-collapsed" />
+                        {/* COLLAPSED */}
+                        <Box sx={{ flexGrow: 1, display: displayCollapsed }}>
+                            <IconButton
+                                size="large"
+                                aria-label="menu"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                                data-testid="navbar-menu-collapsed"
+                            >
+                            {pages.map((page, idx) => (
+                                <MenuItem
+                                    key={idx}
+                                    sx={{ my: 2, display: 'block' }}
+                                    data-testid={`navbar-collapsed-menu-option-${page.label}`}
+                                >
+                                    <a href={page.href} style={{textDecoration: 'inherit', color: 'inherit'}}>{page.label}</a>
+                                </MenuItem>
+                            ))}
+                            </Menu>
+                        </Box>
+                        <Logo display={displayCollapsed} data-testid="navbar-logo-collapsed" />
 
-        </Toolbar>
-                    
-    </AppBar>);
+                </Toolbar>
+            </Container>
+        </AppBar>
+    // </Box>
+    );
 };
 
 Navbar.propTypes = {
