255f4d768da604e300bcc5470e4e51d3aed2439b
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-15 10:51:22 +0100

    added apiSlice containing models object which holds data regarding the getModels request

--- src/services/API/apiSlice.js
+++ src/services/API/apiSlice.js
@@ -1,58 +1,5 @@
 import axios from 'axios';
-
-// Define our single API slice object
-/*
-export const apiSlice = createApi({
-  // The cache reducer expects to be added at `state.api` (already default - this is optional)
-  reducerPath: 'api',
-  
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.o3as.fedcloud.eu/api/v1' }),
-  
-  // The "endpoints" represent operations and requests for this server
-  endpoints: builder => ({
-
-    getApiSpecification: builder.query({
-      query: () => '/openapi.json'
-    }),
-    
-    getModels: builder.query({
-      query: () => '/models'
-    }),
-    getPlots: builder.query({
-      query: () => '/plots'
-    }),
-    
-    getRawPlotData: builder.mutation({
-      query: payload => ({
-        // /data/tco3_return?begin=1959&end=2100&lat_min=-90&lat_max=90
-        url: `/data/${payload.plotType}?begin=1959&end=2100&lat_min=${payload.lat_min}&lat_max=${payload.lat_max}`,
-        method: "POST",
-        body: payload.modelList,
-      })
-    }),
-    
-    getBuiltPlotData: builder.mutation({
-      query: payload => ({
-        
-        url: `/plots/${payload.plotType}?begin=1959&end=2100&lat_min=${payload.lat_min}&lat_max=${payload.lat_max}`,
-        method: "POST",
-        body: payload.modelList,
-      })
-    }),
-    
-  })
-})
-
-// Export the auto-generated hook for the query endpoints
-export const { 
-  useGetApiSpecificationQuery,
-  useGetPlotsQuery,
-  useGetModelsQuery,
-  useGetBuiltPlotDataMutation,
-  useGetRawPlotDataMutation,
- } = apiSlice
-
- */
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const baseURL = "https://api.o3as.fedcloud.eu/api/v1";
 
@@ -78,7 +25,13 @@ export const postData = (plotType, data) => {
 }
 
 export const getModels = (plotType, select) => {
-    return getFromAPI(`/models?ptype=${plotType}&select=${select}`);
+    const hasPlotType = typeof plotType !== "undefined";
+    const hasSelect = typeof select !== "undefined";
+    const hasOne = hasPlotType || hasSelect;
+    const hasBoth = hasPlotType && hasSelect;
+    
+    return getFromAPI(
+        `/models${hasOne ? "?" : ""}${hasPlotType ? `ptype=${plotType}` : ""}${hasBoth ? "&" : ""}${hasSelect ? `select=${select}` : ""}`);
 }
 
 export const postModelsPlotStyle = (plotType) => {
@@ -89,3 +42,39 @@ export const postModelsPlotStyle = (plotType) => {
         }
     );
 }
+
+export const fetchModels = createAsyncThunk('api/fetchModels', async () => {
+    const response = await getModels();
+    return response.data;
+});
+
+const initialState = {
+    models: {
+        status: "idle",
+        error: null,
+        data: [],
+    },
+};
+
+const apiSlice = createSlice({
+    name: "api",
+    initialState,
+    reducers: {
+
+    },
+    extraReducers(builder) {
+        builder
+          .addCase(fetchModels.pending, (state, action) => {
+            state.status = 'loading';
+          })
+          .addCase(fetchModels.fulfilled, (state, action) => {
+            state.status = 'success';
+            state.models.data = action.payload;
+          })
+          .addCase(fetchModels.rejected, (state, action) => {
+            state.status = 'error';
+            state.error = action.error.message;
+          })
+      },
+});
+// reducer(state, action) => newState
