fb67e12ab0725ffe5474c0887c7ca51cd01477cd
simon.vonroenn <simon.vonroenn@web.de>
2022-01-16 13:45:45 +0100

    Begin implementation of DownloadModal.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,36 +1,27 @@
 // Section.js
 /** Stores the name of the LatitudeBandSelector component as a Symbol. */
 export const LBS_Symbol = Symbol("LatitudeBandSelector");
-
 /** Stores the name of the LocationSelector component as a Symbol. */
 export const LS_Symbol = Symbol("LocationSelector");
-
 /** Stores the name of the ModelGroupConfigurator component as a Symbol. */
 export const MGC_Symbol = Symbol("ModelGroupConfigurator");
-
 /** Stores the name of the OffsetConfigurator component as a Symbol. */
 export const OC_Symbol = Symbol("OffsetConfigurator");
-
 /** Stores the name of the PlotNameField component as a Symbol. */
 export const PNF_Symbol = Symbol("PlotNameField");
-
 /** Stores the name of the ReferenceModelSelector component as a Symbol. */
 export const RMS_Symbol = Symbol("ReferenceModelSelector");
-
 /** Stores the name of the ReferenceYearSlider component as a Symbol. */
 export const RYS_Symbol = Symbol("ReferenceYearSlider");
-
 /** Stores the name of the RegionSelector component as a Symbol. */
 export const RS_Symbol = Symbol("RegionSelector");
-
 /** Stores the name of the TimeCheckBoxGroup component as a Symbol. */
 export const TCG_Symbol = Symbol("TimeCheckBoxGroup");
-
 /** Stores the name of the XAxisSlider component as a Symbol. */
 export const XAS_Symbol = Symbol("XAxisSlider");
-
 /** Stores the name of the YAxisSlider component as a Symbol. */
 export const YAS_Symbol = Symbol("YAxisSlider");
+
 // LatitudeBandSelector.js
 export const latitudeBands = [
     {
@@ -66,3 +57,6 @@ export const latitudeBands = [
         value: 'custom'
     },
 ]
+
+// DownloadModal.js
+export const fileFormats = [ Symbol("pdf"), Symbol("png") ];
--- src/views/landingPage/Sidebar/DownloadModal/DownloadModal.js
+++ src/views/landingPage/Sidebar/DownloadModal/DownloadModal.js
@@ -1,30 +1,104 @@
-import * as React from 'react';
+import React from 'react';
+import { FormControl, InputLabel, MenuItem, Modal, Select, Typography, Card, Grid, Button } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { fileFormats } from '../../../../utils/constants';
 
 /**
- * opens a modal to ensure user can downlaod plot
+ * Opens a modal where the user can select the file format and download the plot.
+ *
  * @param {Object} props 
  * @param {boolean} props.isOpen -> whether modal should be visible
  * @param {function} props.onClose -> handles closing the modal
  * @param {function} props.reportError -> enabling to report an error
- * @returns {JSX} a jsx containing a modal with a dropdown to choose the file type and a download button
+ * @returns {JSX.Element} a jsx containing a modal with a dropdown to choose the file type and a download button
  */
 export default function DownloadModal(props) {
 
-    let i = props.onClose;
-    i = props.reportError;
-    i = props.isOpen;
+    /**
+     * The style of the DownloadModal.
+     *
+     * @type {{
+     *          boxShadow: number, transform: string, pr: number, pb: number,
+     *          top: string, bgColor: *, pt: number, left: string, width: string,
+     *          position: string, pl: number
+     *       }} the props
+     */
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '50%',
+        bgColor: useTheme().palette.grey[200],
+        boxShadow: 24,
+        pl: 5,
+        pr: 2,
+        pt: 8,
+        pb: 5,
+    };
 
+    /**
+     * Downloads the plot.
+     *
+     * @todo implement
+     */
+    const downloadPlot = () => {
+        console.log("downloading...");
+    };
 
-    const allAvailableDataFormats = [];
+    /**
+     * @todo move to redux store
+     */
+    const [selectedFileFormat, setSelectedFileFormat] = React.useState('');
 
-    const downloadImg = (imgFormat) => {};
-
-    if (props.isOpen) {
-        return (
-        <>
-            DownloadModal
-        </>
-        );
+    /**
+     * Calls the redux store and changes the selected file format.
+     *
+     * @param {event} event the event that called this function
+     *
+     * @todo connect with redux store
+     */
+    const changeFileFormat = (event) => {
+        setSelectedFileFormat(event.target.value);
     }
-    return (<></>);
+
+    return (
+        <>
+            <Modal
+                open={props.isOpen}
+                onClose={props.onClose}
+            >
+                <Card sx={style}>
+                    <Grid container>
+                        <Grid item xs={3}>
+                            <Typography variant={"h6"} sx={{mt: -1}}>Choose file format:</Typography>
+                        </Grid>
+                        <Grid xs={6}>
+                            <FormControl style={{width: "100%"}} sx={{mt: -3}}>
+                                <Select
+                                    labelId="formatSelectLabel"
+                                    id="formatSelection"
+                                    label="format"
+                                    value={selectedFileFormat}
+                                    onChange={changeFileFormat}
+                                >
+                                    {
+                                        fileFormats.map(
+                                            (elem, idx) => <MenuItem key={idx}>{elem.description}</MenuItem>
+                                        )
+                                    }
+                                </Select>
+                                <InputLabel id="formatSelectLabel">format</InputLabel>
+                            </FormControl>
+                        </Grid>
+                        <Grid xs={3}>
+                            <Button sx={{mx: 5, mt: -2}} variant="outlined" onClick={downloadPlot}>
+                                <Typography>Download</Typography>
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Modal>
+        </>
+    );
 }
