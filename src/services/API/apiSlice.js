ea1149de6811ef26ee58572d657d71bc4387ad23
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-16 15:04:48 +0100

    moved methos to client.js

--- src/services/API/apiSlice.js
+++ src/services/API/apiSlice.js
@@ -1,10 +1,4 @@
-import axios from 'axios';
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { sleep } from '../../utils/mock_dev';
-
-const baseURL = "https://api.o3as.fedcloud.eu/api/v1";
-
-const timeoutVal = 5000;
 
 export const REQUEST_STATE = {
     idle: "idle",
@@ -12,54 +6,6 @@ export const REQUEST_STATE = {
     success: "success",
     error: "error",
 }
-
-const getFromAPI = (endpoint) => {
-    return axios.get(baseURL + endpoint, { timeout: timeoutVal });
-}
-
-const postAtAPI = (endpoint, data) => {
-    return axios.post(baseURL + endpoint, { data }, { timeout: timeoutVal });
-}
-
-export const getPlotTypes = () => {
-  return getFromAPI("/data");
-};
-
-export const postData = (plotType, data) => {
-    return postAtAPI(
-        `/data/${plotType}`,
-        { data }
-    );
-}
-
-export const getModels = (plotType, select) => {
-    const hasPlotType = typeof plotType !== "undefined";
-    const hasSelect = typeof select !== "undefined";
-    const hasOne = hasPlotType || hasSelect;
-    const hasBoth = hasPlotType && hasSelect;
-    
-    return getFromAPI(
-        `/models${hasOne ? "?" : ""}${hasPlotType ? `ptype=${plotType}` : ""}${hasBoth ? "&" : ""}${hasSelect ? `select=${select}` : ""}`);
-}
-
-export const postModelsPlotStyle = (plotType) => {
-    return postAtAPI(
-        '/models/plotstyle',
-        {
-            ptype: plotType
-        }
-    );
-}
-
-export const fetchModels = createAsyncThunk('api/fetchModels', async () => {
-    const response = await getModels();
-    return response.data;
-});
-
-export const fetchPlotTypes = createAsyncThunk('api/fetchPlotTypes', async () => {
-    const response = await getPlotTypes();
-    return response.data;
-});
 
 const initialState = {
     models: {
