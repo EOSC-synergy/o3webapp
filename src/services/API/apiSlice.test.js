f1d4291c3ccba6dbc05dbfe3a1379e6ab319d833
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-16 17:59:32 +0100

    testing successful model request

--- src/services/API/apiSlice.test.js
+++ src/services/API/apiSlice.test.js
@@ -1,4 +1,8 @@
-import { fetchModels, fetchPlotTypes } from "./apiSlice"
+import reducer, { fetchModels, fetchPlotTypes, REQUEST_STATE } from "./apiSlice";
+import axios from 'axios';
+import { configureStore } from "@reduxjs/toolkit";
+
+jest.mock('axios');
 
 describe("fetchModels thunk", () => {
     it('creates the action types', () => {
@@ -7,45 +11,37 @@ describe("fetchModels thunk", () => {
         expect(thunkActionCreator.pending.type).toBe('api/fetchModels/pending')
         expect(thunkActionCreator.fulfilled.type).toBe('api/fetchModels/fulfilled')
         expect(thunkActionCreator.rejected.type).toBe('api/fetchModels/rejected')
-      })
+    });
     
-    it('accepts arguments and dispatches the actions on resolve', async () => {
-        const dispatch = jest.fn()
+    it('updates store accordingly after successful request', async () => {
+        const store = configureStore({
+            reducer: {
+                api: reducer,
+            },
+        });
+
+        const mockedReturnedData = ["modelA", "modelB"];
+
+        const expected = {
+            api: {
+                models: {
+                    status: REQUEST_STATE.success,
+                    error: null,
+                    data: mockedReturnedData,
+                },
+                plotTypes: {
+                    status: REQUEST_STATE.idle,
+                    error: null,
+                    data: [],
+                },
+            },
+        };
+
+        axios.get.mockResolvedValue({data: mockedReturnedData});
+        
+        await store.dispatch(fetchModels());
+
+        expect(store.getState(state => state.api)).toEqual(expected);
+    });    
     
-        let passedArg
-    
-        const result = 42
-        const args = 123
-        let generatedRequestId = ''
-    
-        const thunkActionCreator = createAsyncThunk(
-          'testType',
-          async (arg, { requestId }) => {
-            passedArg = arg
-            generatedRequestId = requestId
-            return result
-          }
-        )
-    
-        const thunkFunction = thunkActionCreator(args)
-    
-        const thunkPromise = thunkFunction(dispatch, () => {}, undefined)
-    
-        expect(thunkPromise.requestId).toBe(generatedRequestId)
-        expect(thunkPromise.arg).toBe(args)
-    
-        await thunkPromise
-    
-        expect(passedArg).toBe(args)
-    
-        expect(dispatch).toHaveBeenNthCalledWith(
-          1,
-          thunkActionCreator.pending(generatedRequestId, args)
-        )
-    
-        expect(dispatch).toHaveBeenNthCalledWith(
-          2,
-          thunkActionCreator.fulfilled(result, generatedRequestId, args)
-        )
-      })
 });
