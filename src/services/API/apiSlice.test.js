d3d5057d4d821e5af32ac61dd2ff7a9c1c363700
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-16 19:38:04 +0100

    added corresponding tests for fetch plot types

--- src/services/API/apiSlice.test.js
+++ src/services/API/apiSlice.test.js
@@ -4,13 +4,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 jest.mock('axios');
 
-describe("fetchModels thunk", () => {
+describe("fetchModels async thunk", () => {
     it('creates the action types', () => {
-        const thunkActionCreator = fetchModels;
-    
-        expect(thunkActionCreator.pending.type).toBe('api/fetchModels/pending')
-        expect(thunkActionCreator.fulfilled.type).toBe('api/fetchModels/fulfilled')
-        expect(thunkActionCreator.rejected.type).toBe('api/fetchModels/rejected')
+        expect(fetchModels.pending.type).toBe('api/fetchModels/pending')
+        expect(fetchModels.fulfilled.type).toBe('api/fetchModels/fulfilled')
+        expect(fetchModels.rejected.type).toBe('api/fetchModels/rejected')
     });
     
     it('updates store accordingly after successful request', async () => {
@@ -38,9 +36,7 @@ describe("fetchModels thunk", () => {
         };
 
         axios.get.mockResolvedValue({data: mockedReturnedData});
-        
         await store.dispatch(fetchModels());
-
         expect(store.getState(state => state.api)).toEqual(expected);
     });
 
@@ -69,12 +65,73 @@ describe("fetchModels thunk", () => {
         };
 
         axios.get.mockReturnValue(Promise.reject(errorMessage));
-        
         await store.dispatch(fetchModels());
-
         expect(store.getState(state => state.api)).toEqual(expected);
+    });    
+});
+
+describe("fetchModels async thunk", () => {
+    it('creates the action types', () => {    
+        expect(fetchPlotTypes.pending.type).toBe('api/fetchPlotTypes/pending')
+        expect(fetchPlotTypes.fulfilled.type).toBe('api/fetchPlotTypes/fulfilled')
+        expect(fetchPlotTypes.rejected.type).toBe('api/fetchPlotTypes/rejected')
     });
     
+    it('updates store accordingly after successful request', async () => {
+        const store = configureStore({
+            reducer: {
+                api: reducer,
+            },
+        });
 
-    
+        const mockedReturnedData = ["tco3_zm", "tco3_return"];
+
+        const expected = {
+            api: {
+                models: {
+                    status: REQUEST_STATE.idle,
+                    error: null,
+                    data: [],
+                },
+                plotTypes: {
+                    status: REQUEST_STATE.success,
+                    error: null,
+                    data: mockedReturnedData,
+                },
+            },
+        };
+
+        axios.get.mockResolvedValue({data: mockedReturnedData});
+        await store.dispatch(fetchPlotTypes());
+        expect(store.getState(state => state.api)).toEqual(expected);
+    });
+
+    it('updates store accordingly after rejected request', async () => {
+        const store = configureStore({
+            reducer: {
+                api: reducer,
+            },
+        });
+
+        const errorMessage = "Timeout of API";
+
+        const expected = {
+            api: {
+                models: {
+                    status: REQUEST_STATE.idle,
+                    error: null,
+                    data: [],
+                },
+                plotTypes: {
+                    status: REQUEST_STATE.error,
+                    error: errorMessage,
+                    data: [],
+                },
+            },
+        };
+
+        axios.get.mockReturnValue(Promise.reject(errorMessage));
+        await store.dispatch(fetchPlotTypes());
+        expect(store.getState(state => state.api)).toEqual(expected);
+    });    
 });
