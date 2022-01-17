cd896b7ca52f265139b3220ae6ce879852764141
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-17 14:40:25 +0100

    testing if api renders received options

--- src/views/landingPage/Sidebar/InputComponents/PlotTypeSelector/PlotTypeSelector.test.js
+++ src/views/landingPage/Sidebar/InputComponents/PlotTypeSelector/PlotTypeSelector.test.js
@@ -2,14 +2,12 @@ import { fetchPlotTypes, REQUEST_STATE } from "../../../../../services/API/apiSl
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PlotTypeSelector from './PlotTypeSelector';
-import { render, fireEvent, screen, findByText, findByTestId } from '@testing-library/react';
+import { render, fireEvent, within } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import userEvent from '@testing-library/user-event';
 import { createTestStore } from '../../../../../store/store'
 import { Provider } from "react-redux";
-import * as redux from 'react-redux'
-
-console.error = jest.fn(); 
-
+import * as redux from 'react-redux';
 
 let store
 describe('plot type selector test', () => {
@@ -37,20 +35,21 @@ describe('plot type selector test', () => {
 
     // Prop types
     it('PlotTypeSelector expects a report error function', () => {
+        console.error = jest.fn();
         render(<Provider store={store}>
             <PlotTypeSelector />
         </Provider>);
         expect(console.error).toHaveBeenCalledTimes(1);
     });
 
-    it('PlotTypeSelector reports error if an error occurred in the fetching of plotTypes', async () => {
+    it('PlotTypeSelector reports error if an error occurred in the fetching of plotTypes', () => {
         const spy = jest.spyOn(redux, 'useSelector')
         
         const errorMessage = "Error Message";
         spy.mockReturnValue(
             { 
                 status: REQUEST_STATE.error,
-                data: ["tco3_zm"],
+                data: [],
                 error: errorMessage,
             }
         );
@@ -62,6 +61,61 @@ describe('plot type selector test', () => {
             <PlotTypeSelector reportError={reportError} />
         </Provider>, div);
         expect(reportError.mock.calls[0][0]).toEqual(errorMessage);
+    });
+
+    it('should render all the returned data from the api', () => {
+        const spy = jest.spyOn(redux, 'useSelector')
+        
+        spy.mockReturnValue( // mock api request
+            { 
+                status: REQUEST_STATE.success,
+                data: ["tco3_zm", "tco3_return"],
+                error: null,
+            }
+        );
+    
+        
+        expect(store.getState().plot.plotId).toEqual("tco3_zm");
+        
+        const { getByRole, getAllByRole } = render(<Provider store={store}>
+            <PlotTypeSelector reportError={() => {}} />
+        </Provider>);
+        
+        const trigger = getByRole('button');
+        fireEvent.mouseDown(trigger);
+
+        const options = getAllByRole('option');
+        expect(options[0]).toEqual("tco3_zm");
+        expect(options[1]).toEqual("tco3_return");
+    });
+
+    it('should update the clicked value in the store', () => {
+        const spy = jest.spyOn(redux, 'useSelector')
+        
+        spy.mockReturnValue( // mock api request
+            { 
+                status: REQUEST_STATE.success,
+                data: ["tco3_zm", "tco3_return"],
+                error: null,
+            }
+        );
+    
+        
+        expect(store.getState().plot.plotId).toEqual("tco3_zm");
+        
+        const { getByRole, getAllByRole } = render(<Provider store={store}>
+            <PlotTypeSelector reportError={() => {}} />
+        </Provider>);
+        
+        const trigger = getByRole('button');
+        fireEvent.mouseDown(trigger);
+
+        const options = getAllByRole('option');
+
+        act(() => {
+            options[1].click(); // click on second option (tco3_return)
+        });
+        expect(store.getState().plot.plotId).toEqual("tco3_return");
     });
 
 });
