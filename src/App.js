c433200d645797f7ffecc2b193fb8e234d5cfd1f
simon.vonroenn <simon.vonroenn@web.de>
2022-01-17 12:04:44 +0100

    Begin implementation of ErrorMessageModal.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from "./components/Footer/Footer";
 import ErrorMessageModal from './components/ErrorMessageModal/ErrorMessageModal';
 import CookieConsentModal from './components/CookieConsentModal/CookieConsentModal';
 import LandingPage from './views/landingPage/LandingPage';
+import PropTypes from "prop-types";
+import PlotTypeSelector from "./views/landingPage/Sidebar/InputComponents/PlotTypeSelector/PlotTypeSelector";
 
 /**
  * Main container of the Webapp
@@ -25,7 +27,7 @@ function App() {
         setErrorModalVisible(true);
         setErrorMessage(msg);
     }
-    
+
     /**
      * closes the error modal
      */
@@ -39,7 +41,7 @@ function App() {
     const onCloseCookieConsentModal = () => {
         setCookieConsentModalVisibility(false);
     }
-    
+
 
     return (
     <>
@@ -48,7 +50,7 @@ function App() {
         <LandingPage error={reportError} />
 
         <Footer error={reportError} />
-        <ErrorMessageModal open={isErrorModalVisible} message={errorMessage} onClose={closeErrorModal} />
+        <ErrorMessageModal isOpen={isErrorModalVisible} message={errorMessage} onClose={closeErrorModal} />
         <CookieConsentModal open={isCookieConsentModalVisible} onClose={onCloseCookieConsentModal} error={reportError} />
     </>
     );
--- src/components/ErrorMessageModal/ErrorMessageModal.js
+++ src/components/ErrorMessageModal/ErrorMessageModal.js
@@ -1,22 +1,66 @@
-import React, { useState } from "react";
+import React from "react";
+import {useTheme} from "@mui/material/styles";
+import {Button, Card, FormControl, Grid, Modal, Typography} from "@mui/material";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 /**
  * displays an error message
  * @param {Object} props 
- * @param {booelan} props.isOpen -> whether the error message modal should be displayed
+ * @param {boolean} props.isOpen -> whether the error message modal should be displayed
  * @param {function} props.onClose -> handles closing of the modal
  * @param {String} props.message -> error message
- * @returns {JSX} a jsx file containing a modal with the given error message
+ * @returns {JSX.Element} a jsx file containing a modal with the given error message
  */
-function ErrorMessageModal(props) {
-    
-    let i = props.isOpen;
-    i = props.onClose;
-    i = props.message;
-    
+export default function ErrorMessageModal(props) {
 
-    return <div>
-    </div>;
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '30%',
+        bgColor: useTheme().palette.grey[200],
+        boxShadow: 24,
+    };
+
+    return (
+        <>
+            <Modal
+                open={props.isOpen}
+                onClose={props.onClose}
+            >
+                <Card sx={style}>
+                    <Grid
+                        container
+                        direction="column"
+                    >
+                        <Grid
+                            item
+                            xs={12} sm={12} md={12} lg={12} xl={12}
+                            key="header"
+                            sx={{backgroundColor:"#e85e6c", display: "flex", justifyContent: "center", py: 5}}
+                        >
+                            <IoCloseCircleOutline color="white" size={100} />
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} key="message" sx={{px: 5, pt: 5}}>
+                            <Typography variant="h3" sx={{textAlign: "center"}}>Ooops!</Typography>
+                            <br />
+                            <Typography>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}</Typography>
+                        </Grid>
+                        <Grid
+                            item
+                            xs={12} sm={12} md={6} lg={6} xl={6}
+                            key="button"
+                            sx={{px: 5, pb: 5}}>
+                            <Button onClick={props.onClose}>Close</Button>
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Modal>
+        </>
+    );
 }
 
-export default ErrorMessageModal;
+ErrorMessageModal.defaultProps = {
+    isOpen: false,
+}
