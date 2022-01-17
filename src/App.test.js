f6bc8ae26e80ad9d382299c483cae6a307d45301
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-17 15:11:09 +0100

    provide redux context for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createTestStore } from './store/store'
+import { Provider } from "react-redux";
 
 jest.mock('react-apexcharts', () => {
   return {
@@ -11,7 +13,18 @@ jest.mock('react-apexcharts', () => {
   }
 })
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+let store;
+describe('test app component', () => {
+  beforeEach(() => {
+    store = createTestStore();
+  });
+  
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Provider store={store}>
+      <App />
+    </Provider>, div);
+  });
+
 });
