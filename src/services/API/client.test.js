adf7d4118ef31601661737471fa8c0ebfe810aec
simon.vonroenn <simon.vonroenn@web.de>
2022-01-17 20:29:41 +0100

    Write test cases for client.js

--- src/services/API/client.test.js
+++ src/services/API/client.test.js
@@ -1,9 +1,29 @@
-import { getModels } from './client';
+import {getModels, postData} from './client';
+import * as axios from 'axios';
 
-describe("connection with api", () => {
-    it('gets the models', async () => {
-        const response = await getModels();
-        expect(response.data).not.toBe(null);
-        console.log(await response.data);
+jest.mock('axios');
+
+describe("Testing the getModels function", () => {
+    it('should format the url correctly', async () => {
+        let url;
+        await getModels();
+        url = axios.get.mock.calls[0][0];
+        expect(url).toEqual("https://api.o3as.fedcloud.eu/api/v1/models");
+        await getModels("tco3_zm");
+        url = axios.get.mock.calls[1][0];
+        expect(url).toEqual("https://api.o3as.fedcloud.eu/api/v1/models?ptype=tco3_zm");
+        await getModels("tco3_zm", "select");
+        url = axios.get.mock.calls[2][0];
+        expect(url).toEqual("https://api.o3as.fedcloud.eu/api/v1/models?ptype=tco3_zm&select=select");
+
+    });
+});
+describe("Testing the postData function", () => {
+    it('should format the url correctly', async () => {
+        let url;
+        await postData("tco3_zm");
+        url = axios.post.mock.calls[0][0];
+        expect(url).toEqual("https://api.o3as.fedcloud.eu/api/v1/data/tco3_zm");
+
     });
 });
