533d5b0e47006f0c2a0ac9da2fb1b6974fcf17c5
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-22 14:26:21 +0100

    providing redux context, updating snapshots accordingly

--- src/views/landingPage/Graph/Graph.test.js
+++ src/views/landingPage/Graph/Graph.test.js
@@ -1,19 +1,30 @@
 import Graph from './Graph'
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { createTestStore } from '../../../store/store'
 
 jest.mock('react-apexcharts', () => {
     return {
-      __esModule: true,
-      default: () => {
-        return <div />
-      },
+        __esModule: true,
+        default: () => {
+            return <div />
+        },
     }
 })
 jest.mock('apexcharts', () => ({ exec: jest.fn(() => { return new Promise((resolve) => { resolve("uri") }) }) }));
 
+let store;
+describe('tests graph component rendering', () => {
+    beforeEach(() => {
+        store = createTestStore();
+    });
 
-test.only('Renders OK', async () => {
-    const { container } = render(<Graph />);
-    expect(container).toMatchSnapshot();
+    it('Renders OK', async () => {
+        const { container } = render(<Provider store={store}>
+            <Graph />
+        </Provider>
+        );
+        expect(container).toMatchSnapshot();
+    });
 });
