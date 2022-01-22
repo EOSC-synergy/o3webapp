069a51097ca203204b2421b29841cbb2207dcf8e
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-22 14:25:03 +0100

    reinserted graph from develop

--- src/views/landingPage/Graph/Graph.js
+++ src/views/landingPage/Graph/Graph.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
+import * as React from 'react';
 import Chart from "react-apexcharts"
 import data from "./default-data.json"
 import settings from "./default-settings.json"
 import { convertToStrokeStyle, colourNameToHex } from "../../../utils/optionsFormatter"
 import {useSelector} from 'react-redux'
-import { selectPlotId } from '../../../store/plotSlice/plotSlice';
-import { REQUEST_STATE, selectActivePlotData } from '../../../services/API/apiSlice';
-import { Spinner } from '../../../components/Spinner/Spinner';
-import { Typography } from '@mui/material';
-import { formatDataBasedOnPlotId } from "../../../utils/optionsFormatter";
+import { useGetRawPlotDataMutation } from '../../../services/API/apiSlice';
+import { selectCurrentModelGroups } from '../../../store/modelsSlice/modelsSlice';
+import { selectCurrentPlotId, selectCurrentPlotType, selectCurrentSettings, selectPlotTitle } from '../../../store/plotSlice/plotSlice';
+import { selectCurrentReferenceSettings } from '../../../store/referenceSlice';
+// import {calculatePlotSeries} from "../../../utils/math"
 
 /**
  * Static generation of the years on the x-axis, this will be fetched from the
@@ -46,37 +46,6 @@ settings.options.stroke.dashArray.push(...strokes)
 settings.options.stroke.width.push(...lineWidth)
 settings.series.push(...ySeries)
 
-const prepareData = (data, plotId) => {
-    const transformedData = formatDataBasedOnPlotId(data, plotId);
-    // calculate statistical values
-    // merge model settings
-    // merge statistical values
-    return transformedData;
-}
-
-const renderCorrectChartComponent = (plotId, data) => {
-    // mapping: from plotId to chart type required (apexcharts component)
-    // select compmonent: return comp, this gets rendered in Graph if data is there
-    if (plotId === "tco3_zm") {
-        const transformed = prepareData(data, plotId);
-        settings.options.stroke.width = transformed.lineWidth;
-        settings.options.stroke.dashArray = transformed.strokes;
-        settings.series = transformed.ySeries;
-        settings.options.xaxis.categories = transformed.xAxis;
-        settings.options.colors = transformed.colors;
-        return <Chart
-            options={settings.options}
-            series={settings.series}
-            type={"line"}
-            height={"60%"}
-            width={"95%"}
-        />
-    } else if (plotId === "tco3_return") {
-        return <Typography>To be implemented...</Typography>
-    }
-    
-}
-
 /**
  * Currently there is no dynamic data linking. The graph will always
  * render the data from default-data.json in this folder. This is
@@ -88,39 +57,19 @@ const renderCorrectChartComponent = (plotId, data) => {
  *          the apexcharts library
  */
 function Graph(props) {
+    settings.options.title.text = useSelector(selectPlotTitle);
+    const copySettings = JSON.parse(JSON.stringify(settings)); 
 
-    const plotId = useSelector(selectPlotId);
-    const activeData = useSelector(state => selectActivePlotData(state, plotId));
-
-    useEffect(() => { 
-        // note: this is important, because we should only "propagate" the error to the top
-        // if this component has finished rendering, causing no <em>side effects</em> in
-        // its rendering process 
-        if (activeData.status === REQUEST_STATE.error) {
-            props.reportError(activeData.error);
-        }
-    })
-
-    if (activeData.status === REQUEST_STATE.loading || activeData.status === REQUEST_STATE.idle) {
-        return <Spinner text={"loading data"} size={"8em"}></Spinner>
-
-    } else if (activeData.status === REQUEST_STATE.error) {
-        return <Typography>An error occurred, please try to reload the site</Typography>;
-
-    } else if (activeData.status === REQUEST_STATE.success) {
-        return renderCorrectChartComponent(plotId, activeData.data);
-        /*
+    return (<>
+        {/* for the OCTS Plot */}
         <Chart
-            options={settings.options}
-            series={settings.series}
-            type={"line"}
-            height={"60%"}
-        />
-        */
-    };
-
-    // this "case" should not happen
-    return <Typography>CRITICAL: an internal error occurred that shouldn't happen!</Typography>;
+                options={copySettings.options}
+                series={copySettings.series}
+                type={"line"}
+                height={"600px"}
+            />
+        
+    </>);
 }
 
 export default Graph;
