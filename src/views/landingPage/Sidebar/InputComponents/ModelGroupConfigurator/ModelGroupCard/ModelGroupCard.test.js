59e08e639dcaf367781a53abdfe9ba19973fe076
Jana-Z <github3@jana-zeller.de>
2022-01-24 21:33:17 +0100

    added tests

--- src/views/landingPage/Sidebar/InputComponents/ModelGroupConfigurator/ModelGroupCard/ModelGroupCard.test.js
+++ src/views/landingPage/Sidebar/InputComponents/ModelGroupConfigurator/ModelGroupCard/ModelGroupCard.test.js
@@ -1,1 +1,38 @@
-test.todo('some test to be written in the future');
+import { render, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ModelGroupCard, { getGroupName } from './ModelGroupCard';
+
+
+describe('test ModelGroupCard rendering', () => {
+    
+    it('renders without crashing', () => {
+        render(<ModelGroupCard modelGroupId={1} />);
+    });
+
+    
+    it('renders correctly', () => {
+        const { container } = render(<ModelGroupCard modelGroupId={1} />);
+        expect(container).toMatchSnapshot();
+    });
+
+    it('renders modelGroup Name', () => {
+        const groupId = 1;
+        const { container } = render(<ModelGroupCard modelGroupId={groupId} />);
+        const groupName = getGroupName(groupId);
+        expect(container).toHaveTextContent(groupName);
+    });
+
+    it('raises a console.error function if a required prop is not provided', () => {
+        console.error = jest.fn();
+        render(<ModelGroupCard />);
+        expect(console.error).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('test addModelGroupModal functionality', () => {
+
+    test.todo('test toggle {median, mean, derivative, percentile}');
+    test.todo('test toggle group visibility');
+    test.todo('test that edit modal opens');
+
+});
