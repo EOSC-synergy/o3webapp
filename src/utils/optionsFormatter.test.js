823e7bf64e2714aa19e0f3aec8c82cc4fde47f7d
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-27 16:53:24 +0100

    testing tco3_zm data transformation

--- src/utils/optionsFormatter.test.js
+++ src/utils/optionsFormatter.test.js
@@ -1,30 +1,52 @@
-import { IMPLICIT_YEAR_LIST, START_YEAR } from "./constants";
-import { normalizeArray } from "./optionsFormatter";
+import { IMPLICIT_YEAR_LIST, O3AS_PLOTS, START_YEAR } from "./constants";
+import { normalizeArray, preTransformApiData } from "./optionsFormatter";
 
 describe("testing optionsFormatter functionality", () => {
+    const spacedYearArray = [...Array(10).keys()].map(number => `${START_YEAR + 2 * number}`);
+    const valueArray = [...Array(10).keys()] // arbitrary values 0...9
+    const expectedNormalize = []
+    for (let year of IMPLICIT_YEAR_LIST) {
+        const idx = spacedYearArray.indexOf(year)
+        if (idx !== -1) {
+            expectedNormalize.push(valueArray[idx]);
+        } else {
+            expectedNormalize.push(null);
+        }
+    }
 
     it('should normalize arrays as expected', () => {
         // generate an array of years describing the valid datapoints. 
         // the normalized array should contain the valid datapoints at
         // the index of where the implicit year list lists this year.
-        const spacedYearArray = [...Array(10).keys()].map(number => `${START_YEAR + 2 * number}`);
-        const valueArray = [...Array(10).keys()] // arbitrary values 0...9
-        const expected = []
-        for (let year of IMPLICIT_YEAR_LIST) {
-            const idx = spacedYearArray.indexOf(year)
-            if (idx !== -1) {
-                expected.push(valueArray[idx]);
-            } else {
-                expected.push(null);
-            }
-        }
-        expect(normalizeArray(spacedYearArray, valueArray)).toEqual(expected);
+        
+        expect(normalizeArray(spacedYearArray, valueArray)).toEqual(expectedNormalize);
     });
 
     
-    it('should preTranform the api data correctly', () => {
+    describe("preTransform for all plot types", () => {
+        it('should pre-tranform the api data for tco3_zm correctly', () => {
+            const apiData = [{
+                model: "modelA",
+                plotstyle: "plotstyle",
+                x: spacedYearArray,
+                y: valueArray,
+            }];
+    
+            expect(
+                preTransformApiData({plotId: O3AS_PLOTS.tco3_zm, data: apiData})
+            ).toEqual({
+                modelA: {
+                    plotStyle: "plotstyle",
+                    data: expectedNormalize,
+                }
+            });
+    
+        });
 
-    });
+        it('should pre-transform the api data for tco3_return correctly', () => {
+            
+        });
+    })
 
     it('calculates the correct boxplot values', () => {
 
