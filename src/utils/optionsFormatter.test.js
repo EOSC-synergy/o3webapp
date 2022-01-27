82e9e894b2b11cb58ec744014cf53f2da2388a6b
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-27 16:35:19 +0100

    testing normalize array function

--- src/utils/optionsFormatter.test.js
+++ src/utils/optionsFormatter.test.js
@@ -1,9 +1,24 @@
-import {normali} from "./optionsFormatter";
+import { IMPLICIT_YEAR_LIST, START_YEAR } from "./constants";
+import { normalizeArray } from "./optionsFormatter";
 
 describe("testing optionsFormatter functionality", () => {
 
     it('should normalize arrays as expected', () => {
-
+        // generate an array of years describing the valid datapoints. 
+        // the normalized array should contain the valid datapoints at
+        // the index of where the implicit year list lists this year.
+        const spacedYearArray = [...Array(10).keys()].map(number => `${START_YEAR + 2 * number}`);
+        const valueArray = [...Array(10).keys()] // arbitrary values 0...9
+        const expected = []
+        for (let year of IMPLICIT_YEAR_LIST) {
+            const idx = spacedYearArray.indexOf(year)
+            if (idx !== -1) {
+                expected.push(valueArray[idx]);
+            } else {
+                expected.push(null);
+            }
+        }
+        expect(normalizeArray(spacedYearArray, valueArray)).toEqual(expected);
     });
 
     
