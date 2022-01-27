0e4c1bb8c39eb5200c2ddc7113cb3ea9fa3b3e57
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-27 16:00:03 +0100

    prepared testing

---
+++ src/utils/optionsFormatter.test.js
@@ -0,0 +1,49 @@
+
+
+describe("testing optionsFormatter functionality", () => {
+
+    it('should normalize arrays as expected', () => {
+
+    });
+
+    
+    it('should preTranform the api data correctly', () => {
+
+    });
+
+    it('calculates the correct boxplot values', () => {
+
+    });
+
+    it('generates the the tco3_zm series', () => {
+
+    });
+
+    it('generates the tco3_return series', () => {
+
+    });
+
+    it('extracts all models which should be included', () => {
+
+    });
+
+    it('generates the series accordingly', () => {
+
+    });
+
+
+    it('returns the correct options formatted correctly', () => {
+
+    });
+
+    it('converts the color name to hex codes', () => {
+
+    });
+    
+    it('converts the stroke style to to apexcharts syntax', () => {
+
+    });
+
+
+
+});
