293536d4b6783d7bdf3d1d34430bcbec3e36c56f
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-01-27 20:06:16 +0100

    added jsdoc and tests to math service

--- src/services/math/math.js
+++ src/services/math/math.js
@@ -1,21 +1,48 @@
-export function generatePlotSeries(models) {
-    // Calculate
-}
+/**
+ * This functions accepts an array, sorts it in-place and returns a reference to the same array.
+ * 
+ * @param {array} arr of numbers
+ * @returns the array sorted ascendingly
+ */
+export const asc = arr => arr.sort((a, b) => a - b);
 
-// sort array ascending
-const asc = arr => arr.sort((a, b) => a - b);
+/**
+ * This function accepts an array of numbers and sums over its elements.
+ * 
+ * @param {array} arr of numbers
+ * @returns the sum of all elements
+ */
+export const sum = arr => arr.reduce((a, b) => a + b, 0);
 
-const sum = arr => arr.reduce((a, b) => a + b, 0);
+/**
+ * This function calculates the mean of a given array of numbers
+ * 
+ * @param {array} arr of numbers
+ * @returns the mean of all elements in the given array
+ */
+export const mean = arr => sum(arr) / arr.length;
 
-const mean = arr => sum(arr) / arr.length;
 
-// sample standard deviation
-const std = (arr) => {
+/**
+ * This function calculates the standard deviation of a given array.
+ * 
+ * @param {array} arr of numberes
+ * @returns the standard deviation of the given array
+ */
+export const std = (arr) => {
     const mu = mean(arr);
     const diffArr = arr.map(a => (a - mu) ** 2);
-    return Math.sqrt(sum(diffArr) / (arr.length - 1));
+    return Math.sqrt(sum(diffArr) / arr.length);
 };
 
+/**
+ * Calculate quantiles so, that the calculation of 
+ * quartiles are based on method 2: https://en.wikipedia.org/wiki/Quartile
+ * 
+ * @param {array} arr the array to calculate the quantile of
+ * @param {number} q the quantile 
+ * @returns the number corresponding to the given quantile
+ */
 const quantile = (arr, q) => {
     const sorted = asc(arr);
     const pos = (sorted.length - 1) * q;
@@ -28,10 +55,35 @@ const quantile = (arr, q) => {
     }
 };
 
+
+/**
+ * Returns the lower quartile of the given array
+ * 
+ * @param {array} arr the given array
+ * @returns the lower quartile
+ */
 export const q25 = arr => quantile(arr, .25);
 
+/**
+ * Returns the middle quartile of the given array
+ * 
+ * @param {array} arr the given array
+ * @returns the middle quartile
+ */
 const q50 = arr => quantile(arr, .50);
 
+/**
+ * Returns the lower quartile of the given array
+ * 
+ * @param {array} arr the given array
+ * @returns the upper quartile
+ */
 export const q75 = arr => quantile(arr, .75);
 
+/**
+ * Returns the lmedian of the given array
+ * 
+ * @param {array} arr the given array
+ * @returns the median
+ */
 export const median = arr => q50(arr);
