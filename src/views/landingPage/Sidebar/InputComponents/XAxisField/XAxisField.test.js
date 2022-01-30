6c5b53fcb441a177f57e1ee3446156ce1e4adace
simon.vonroenn <simon.vonroenn@web.de>
2022-01-30 17:33:35 +0100

    Add basic test cases for XAxisField.js and YAxisField.js

--- src/views/landingPage/Sidebar/InputComponents/XAxisField/XAxisField.test.js
+++ src/views/landingPage/Sidebar/InputComponents/XAxisField/XAxisField.test.js
@@ -1,1 +1,29 @@
-test.todo('some test to be written in the future');
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux'
+import XAxisField from './XAxisField';
+import { createTestStore } from "../../../../../store/store"
+
+let store;
+beforeEach(() => {
+    store = createTestStore();
+});
+
+it('renders without crashing', () => {
+    render(<>
+        <Provider store={store}>
+            <XAxisField reportError={() => {}} />
+        </Provider>
+    </>)
+});
+
+it('renders correctly', () => {
+
+    const { container } = render(<>
+        <Provider store={store}>
+            <XAxisField reportError={() => {}} />
+        </Provider>
+    </>);
+
+    expect(container).toMatchSnapshot();
+});
