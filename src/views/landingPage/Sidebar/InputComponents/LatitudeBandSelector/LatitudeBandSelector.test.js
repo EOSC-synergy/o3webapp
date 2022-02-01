f70aa14d2f0d3d0b32077d06317aeff9a7da0788
simon.vonroenn <simon.vonroenn@web.de>
2022-02-01 21:49:41 +0100

    Update LatitudeBandSelector.test.js

--- src/views/landingPage/Sidebar/InputComponents/LatitudeBandSelector/LatitudeBandSelector.test.js
+++ src/views/landingPage/Sidebar/InputComponents/LatitudeBandSelector/LatitudeBandSelector.test.js
@@ -1,37 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux'
 import LatitudeBandSelector from './LatitudeBandSelector';
-import renderer from 'react-test-renderer';
-import "@testing-library/jest-dom/extend-expect";
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import {shallow, configure, mount} from 'enzyme';
+import { createTestStore } from "../../../../../store/store"
 
-describe('LatitudeBandSelector component', () => {
-    configure({adapter: new Adapter()});
-    let wrapper;
-    beforeEach(() => {
-        wrapper = shallow(<LatitudeBandSelector />);
-    });
+let store;
+beforeEach(() => {
+    store = createTestStore();
+});
 
-    it('should render without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(wrapper, div);
-    });
+it('renders without crashing', () => {
+    render(<>
+        <Provider store={store}>
+            <LatitudeBandSelector reportError={() => {}} />
+        </Provider>
+    </>)
+});
 
-    // Snapshot test
-    it('should render correctly from config file', () => {
-        const tree = renderer.create(wrapper).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-    /*
-    it('should update `isCustomizable` state', () => {
-        let wrapped = mount(<LatitudeBandSelector />);
-        let wrappedFind = wrapped.find('#latitudeBandSelector');
-        //console.log(wrappedFind.debug());
-        wrappedFind.first().props().onChange({ target: { value: 'custom' } });
-        wrappedFind.first().simulate('change', { target: { value: 'custom' } });
-        expect(wrapped.find('customLatitudeBandInput').at(0));
-        expect(wrapped.find('customLatitudeBandInput').at(1));
-    });
-     */
+it('renders correctly', () => {
+
+    const { container } = render(<>
+        <Provider store={store}>
+            <LatitudeBandSelector reportError={() => {}} />
+        </Provider>
+    </>);
+
+    expect(container).toMatchSnapshot();
 });
