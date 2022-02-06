647163f0fe3cb5d4e19c84f7b85ccfe360745f31
Furkan <fcevik2001@gmail.com>
2022-02-06 15:32:07 +0100

    Added Legal Notice Links into the Pdf.

--- src/services/pdf/pdfCreator.js
+++ src/services/pdf/pdfCreator.js
@@ -4,22 +4,10 @@ import { O3AS_PLOTS } from "../../utils/constants";
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-const legalNoticeHeading = "ACCEPTABLE USE POLICY AND CONDITIONS OF USE";
-
-const legalNotice = `This Acceptable Use Policy and Conditions of Use (“AUP”) defines the rules and conditions that govern your access to and use (including transmission, processing, and storage of data) of the resources and services (“Services”) as granted by the department Data Analytics, Access and Applications (D3A) from the Steinbuch Centre for Computing (SCC) of Karlsruhe Institute of Technology (KIT), located at Hermann-von-Helmholtz-Platz 1, 76344 Eggenstein-Leopoldshafen (the “Provider”) for the purpose of retrieving the ozone data and producing high-quality figures for the ozone assessment.`;
-
-const points = [
-  "You shall only use the Services in a manner consistent with the purposes and limitations described above; you shall show consideration towards other users including by not causing harm to the Services; you have an obligation to collaborate in the resolution of issues arising from your use of the Services.",
-  "You shall only use the Services for lawful purposes and not breach, attempt to breach, nor circumvent administrative or security controls.",
-  "You shall respect intellectual property and confidentiality agreements.",
-  "You shall protect your access credentials (e.g. passwords, private keys or multi-factor tokens); no intentional sharing is permitted.",
-  "You shall keep your registered information correct and up to date.",
-  "You shall promptly report known or suspected security breaches, credential compromise, or misuse to the security contact stated below; and report any compromised credentials to the relevant issuing authorities.",
-  "Reliance on the Services shall only be to the extent specified by any applicable service level agreements listed below. Use without such agreements is at your own risk.",
-  "Your personal data will be processed in accordance with the privacy statements referenced below.",
-  "Your use of the Services may be restricted or suspended, for administrative, operational, or security reasons, without prior notice and without compensation.",
-  'You shall provide appropriate acknowledgement and citation for your use of the resources/services and the original data as described in "Data policies, references and acknowledgments for the O3as services and original data".',
-  "If you violate these rules, you may be liable for the consequences, which may include your account being suspended and a report being made to your home organisation or to law enforcement.",
+const legalNoticeLink = [
+  "Terms of Use Link: https://o3as.data.kit.edu/policies/terms-of-use.html",
+  "Privacy Policy Link: https://o3as.data.kit.edu/policies/privacy-policy.html",
+  "How to Acknowledge Link: https://o3as.data.kit.edu/policies/how-to-acknowledge.html",
 ];
 
 export async function getBase64Image() {
@@ -33,6 +21,7 @@ export async function getBase64Image() {
 }
 
 function addViewbox(str) {
+  console.log(str);
   const lastIndex = str.indexOf("height") + 10;
   const offset = str.substring(lastIndex).indexOf('"');
   const insertAt = offset + lastIndex + 2;
@@ -45,11 +34,15 @@ function addViewbox(str) {
 }
 
 export async function downloadGraphAsPDF(plotId, fileName) {
-  const svgAsBase64 = await getBase64Image();
+  //const svgAsBase64 = await getBase64Image();
+    
+  const svgElement = document.querySelector(".apexcharts-svg");
+  const svgAsBase64 =
+    "data:image/svg+xml;charset=utf-8," +
+    encodeURIComponent(svgElement.outerHTML);
 
   let docDefinition = null;
 
-  console.log(plotId);
 
   if (plotId === O3AS_PLOTS.tco3_zm || plotId === O3AS_PLOTS.tco3_return) {
     docDefinition = {
@@ -64,33 +57,25 @@ export async function downloadGraphAsPDF(plotId, fileName) {
           svg: addViewbox(svgAsBase64),
           fit: [770, 350],
         },
+        '\n',
+        '\n',
+        '\n',
         {
           text: "List of used models:",
           fontSize: 14,
         },
+        '\n',
         {
           // ul: models
         },
+        '\n',
+        '\n',
+        '\n',
         {
-          text: legalNoticeHeading,
-          fontSize: 14,
-        },
-        legalNotice,
-        {
-          ol: points,
-        },
+          fontSize: 11,
+          ul: legalNoticeLink,
+        }
       ],
-      pageBreakBefore: function (
-        currentNode,
-        followingNodesOnPage,
-        nodesOnNextPage,
-        previousNodesOnPage
-      ) {
-        return (
-          currentNode.text == legalNoticeHeading ||
-          currentNode.text == "List of used models:"
-        );
-      },
     };
   } else {
     throw `the given plot id "${plotId}" is not defined`;
