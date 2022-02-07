dac8f452cb1f7f00c0e145872905c605a8b40608
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-02-07 18:22:38 +0100

    trying stateful variable

--- src/views/landingPage/Sidebar/InputComponents/PlotNameField/PlotNameField.js
+++ src/views/landingPage/Sidebar/InputComponents/PlotNameField/PlotNameField.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux"
-import { setTitle } from "../../../../../store/plotSlice/plotSlice";
+import { selectPlotId, setTitle } from "../../../../../store/plotSlice/plotSlice";
 import { Divider, Typography, Box, FormControl, TextField} from '@mui/material';
 import { useSelector } from 'react-redux';
 import { selectPlotTitle } from "../../../../../store/plotSlice/plotSlice";
 import PropTypes from 'prop-types';
-import { PLOT_NAME_MAX_LEN } from "../../../../../utils/constants"
+import { O3AS_PLOTS, PLOT_NAME_MAX_LEN } from "../../../../../utils/constants"
 
 /**
  * Enables the user to rename and change the plot title.
@@ -13,7 +13,7 @@ import { PLOT_NAME_MAX_LEN } from "../../../../../utils/constants"
  * @param {function} props.reportError - function for error handling
  * @returns {JSX} a textfield to change the plotname
  */
-function PlotNameField(props) {
+function PlotNameField() {
     
     /** The title that should be displayed above the TextField. */
     const componentTitle = "PLOT NAME";
@@ -24,20 +24,31 @@ function PlotNameField(props) {
     /** Dispatcher to dispatch the plot name change action. */
     const dispatch = useDispatch();
 
+    const plotId = useSelector(selectPlotId);
+    const plotTitle = useSelector(selectPlotTitle);
+    const [title, setTitle] = useState(plotTitle);
+
+    useEffect(() => {
+        setTitle(plotTitle);
+    }, [plotId]);
 
     /** The current plot title from the store */
-    const plotTitle = useSelector(selectPlotTitle);
+    console.log(plotTitle)
 
     /** Handles the change if the text in TextField is modified. */
     const updatePlotName = (event) => {
         if (event.target.value.length > PLOT_NAME_MAX_LEN) {
             event.target.value = event.target.value.slice(0, PLOT_NAME_MAX_LEN);
         } 
-
+        console.log({title: event.target.value});
         dispatch(setTitle({title: event.target.value}));
     }
-    
 
+    const updatePlotTitleState = (event) => {
+        setTitle(event.target.value);
+    }
+    
+    let isUpdating = false;
     return ( <>
         <Divider><Typography>{componentTitle}</Typography></Divider>
         <Box sx={{paddingLeft: '8%', paddingRight: '8%', paddingTop: '3%', paddingBottom: '3%'}}>
@@ -47,11 +58,15 @@ function PlotNameField(props) {
                     id="standard-basic"
                     label={textFieldLabel}
                     variant="standard"
+                    value={title}
                     defaultValue={plotTitle}
                     onBlur={updatePlotName}
+                    onChange={updatePlotTitleState}
                     onKeyPress={(event) => {
-                        if (event.key === "Enter") {
+                        if (event.key === "Enter" && !isUpdating) {
+                            isUpdating = true;
                             updatePlotName(event);
+                            isUpdating = false;
                         }
                     }}
                 />
