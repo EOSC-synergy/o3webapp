ae56718275a25bf1c71edc75547054207d05a20b
simon.vonroenn <simon.vonroenn@web.de>
2022-02-22 18:17:51 +0100

    Remove console log

--- src/utils/optionsFormatter/optionsFormatter.test.js
+++ src/utils/optionsFormatter/optionsFormatter.test.js
@@ -142,8 +142,7 @@ describe("testing optionsFormatter functionality", () => {
                 data[key].plotStyle.linestyle = "solid";
                 data[key].data = Array(141).fill(0);
             })
-            const series = generateSeries({plotId: O3AS_PLOTS.tco3_zm, data: data, modelsSlice: modelsSlice})
-            console.log(series);
+            const series = generateSeries({plotId: O3AS_PLOTS.tco3_zm, data: data, modelsSlice: modelsSlice});
             expect(series).toEqual(dataExpected);
         });
     });
