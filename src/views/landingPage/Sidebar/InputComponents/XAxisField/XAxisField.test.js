d389c04fb02836771c1d545851e70a72fb58cfff
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-03-10 10:47:25 +0100

    extended testcases to check for error handling

--- src/views/landingPage/Sidebar/InputComponents/XAxisField/XAxisField.test.js
+++ src/views/landingPage/Sidebar/InputComponents/XAxisField/XAxisField.test.js
@@ -101,19 +101,20 @@ describe('test error handling functionality', () => {
         expect(container).toHaveTextContent(`>${END_YEAR}`); // expect that error is displayed
     });
 
-    it("doesn't update if the value on the left is greater than the value on the right", () => {
+    it("doesn't update if LEFT >= RIGHT", () => {
+        const previousState = store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.maxX;
 
         const { container, getByTestId } = render(
             <Provider store={store}>
                 <XAxisField />
             </Provider>
         );     
-        const inputField = getByTestId("XAxisField-right-input");
-        fireEvent.change(inputField, {target: {value: END_YEAR + 1}}); // change input
-        expect(store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.maxX).toEqual(previousState); // no update took place
-        expect(container).toHaveTextContent(`>${END_YEAR}`); // expect that error is displayed
+        const inputFieldLeft = getByTestId("XAxisField-left-input");
+        const inputFieldRight = getByTestId("XAxisField-right-input");
+        fireEvent.change(inputFieldLeft, {target: {value: 2060}}); // change input
+        fireEvent.change(inputFieldRight, {target: {value: 2060}}); // change input (should have no effect)
+        expect(store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.minX).toEqual(2060); // assure left got updated correctly
+        expect(store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.maxX).toEqual(previousState); // no update took place on the right side
+        expect(container).toHaveTextContent("min>=max"); // expect that error is displayed
     });
-
-   
-
 });
