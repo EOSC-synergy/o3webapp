e61f71e8fccdfda1a4760e290ec73db271fce0bb
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-03-10 10:56:04 +0100

    restructured existing tests

--- src/views/landingPage/Sidebar/InputComponents/YAxisField/YAxisField.test.js
+++ src/views/landingPage/Sidebar/InputComponents/YAxisField/YAxisField.test.js
@@ -4,26 +4,28 @@ import { Provider } from 'react-redux'
 import YAxisField from './YAxisField';
 import { createTestStore } from "../../../../../store/store"
 
-let store;
-beforeEach(() => {
-    store = createTestStore();
-});
-
-it('renders without crashing', () => {
-    render(<>
-        <Provider store={store}>
-            <YAxisField reportError={() => {}} />
-        </Provider>
-    </>)
-});
-
-it('renders correctly', () => {
-
-    const { container } = render(<>
-        <Provider store={store}>
-            <YAxisField reportError={() => {}} />
-        </Provider>
-    </>);
-
-    expect(container).toMatchSnapshot();
+describe("tests basic rendering", () => {
+    let store;
+    beforeEach(() => {
+        store = createTestStore();
+    });
+    
+    it('renders without crashing', () => {
+        render(<>
+            <Provider store={store}>
+                <YAxisField reportError={() => {}} />
+            </Provider>
+        </>)
+    });
+    
+    it('renders correctly', () => {
+    
+        const { container } = render(<>
+            <Provider store={store}>
+                <YAxisField reportError={() => {}} />
+            </Provider>
+        </>);
+    
+        expect(container).toMatchSnapshot();
+    });
 });
