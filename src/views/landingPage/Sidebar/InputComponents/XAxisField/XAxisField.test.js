9a446ece4e04a0998e0ba8d6962b9fbc5008da4b
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-03-10 10:40:16 +0100

    tests for too small / too large components

--- src/views/landingPage/Sidebar/InputComponents/XAxisField/XAxisField.test.js
+++ src/views/landingPage/Sidebar/InputComponents/XAxisField/XAxisField.test.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import XAxisField from './XAxisField';
 import { createTestStore } from "../../../../../store/store"
 import {fireEvent} from '@testing-library/dom';
+import { END_YEAR, START_YEAR } from '../../../../../utils/constants';
 
 describe('tests basic rendering', () => {
     let store;
@@ -41,7 +42,7 @@ describe('test functionality redux', () => {
         // value before change
         expect(store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.minX).toEqual(1960);
 
-        const { container, getByTestId } = render(
+        const { getByTestId } = render(
             <Provider store={store}>
                 <XAxisField />
             </Provider>
@@ -55,7 +56,7 @@ describe('test functionality redux', () => {
         // value before change
         expect(store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.maxX).toEqual(2100);
 
-        const { container, getByTestId } = render(
+        const { getByTestId } = render(
             <Provider store={store}>
                 <XAxisField />
             </Provider>
@@ -66,3 +67,53 @@ describe('test functionality redux', () => {
     });
 });
 
+describe('test error handling functionality', () => {
+    let store;
+    beforeEach(() => {
+        store = createTestStore();
+    });
+
+    it("doesn't update if the value is smaller than START_YEAR (1960)", () => {
+        const previousState = store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.minX;
+
+        const { container, getByTestId } = render(
+            <Provider store={store}>
+                <XAxisField />
+            </Provider>
+        );     
+        const inputField = getByTestId("XAxisField-left-input");
+        fireEvent.change(inputField, {target: {value: START_YEAR - 1}}); // change input
+        expect(store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.minX).toEqual(previousState); // no update took place
+        expect(container).toHaveTextContent(`<${START_YEAR}`); // expect that error is displayed
+    });
+
+    it("doesn't update if the value is greater than END_YEAR (1960)", () => {
+        const previousState = store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.maxX;
+
+        const { container, getByTestId } = render(
+            <Provider store={store}>
+                <XAxisField />
+            </Provider>
+        );     
+        const inputField = getByTestId("XAxisField-right-input");
+        fireEvent.change(inputField, {target: {value: END_YEAR + 1}}); // change input
+        expect(store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.maxX).toEqual(previousState); // no update took place
+        expect(container).toHaveTextContent(`>${END_YEAR}`); // expect that error is displayed
+    });
+
+    it("doesn't update if the value on the left is greater than the value on the right", () => {
+
+        const { container, getByTestId } = render(
+            <Provider store={store}>
+                <XAxisField />
+            </Provider>
+        );     
+        const inputField = getByTestId("XAxisField-right-input");
+        fireEvent.change(inputField, {target: {value: END_YEAR + 1}}); // change input
+        expect(store.getState().plot.plotSpecificSettings.tco3_zm.displayXRange.years.maxX).toEqual(previousState); // no update took place
+        expect(container).toHaveTextContent(`>${END_YEAR}`); // expect that error is displayed
+    });
+
+   
+
+});
