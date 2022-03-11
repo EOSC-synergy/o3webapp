f13537cf9d170694c0ef9a3acbf7da0967ebb39c
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-03-11 12:49:07 +0100

    restructed tests in suits

--- src/views/landingPage/Sidebar/InputComponents/TimeCheckboxGroup/TimeCheckboxGroup.test.js
+++ src/views/landingPage/Sidebar/InputComponents/TimeCheckboxGroup/TimeCheckboxGroup.test.js
@@ -11,7 +11,7 @@ beforeEach(() => {
 });
 
 describe("tests basic rendering", () => {
-    
+
     it('renders without crashing', () => {
         render(<>
             <Provider store={store}>
@@ -49,68 +49,66 @@ describe("tests basic rendering", () => {
 
 });
 
-it('selects a month correctly', () => {
+describe("tests redux functionality", () => {
+    it('selects a month correctly', () => {
+        const { getByTestId } = render(<>
+            <Provider store={store}>
+                <TimeCheckBoxGroup reportError={() => {}} />
+            </Provider>
+            </>);
+        
+        expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]); // assert: if it fails the initial state may have changed
+        fireEvent.click(getByTestId("CheckboxMonth2"));
+        expect(store.getState().plot.generalSettings.months).toEqual([1, 12]);
+        fireEvent.click(getByTestId("CheckboxMonth2"));
+        expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]);
+        
+    })
     
+    it('selects a season correctly', () => {
+        
+        const { getByTestId } = render(<>
+            <Provider store={store}>
+                <TimeCheckBoxGroup reportError={() => {}} />
+            </Provider>
+            </>);
+        
+        expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]); // assert: if it fails the initial state may have changed
+    })
     
-    const { getByTestId } = render(<>
-        <Provider store={store}>
-            <TimeCheckBoxGroup reportError={() => {}} />
-        </Provider>
-        </>);
+    it('selects the "All Year" checkbox correctly', () => {
+        
+        const { getByTestId } = render(<>
+            <Provider store={store}>
+                <TimeCheckBoxGroup reportError={() => {}}/>
+            </Provider>
+            </>);
+        
+        expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]); // assert: if it fails the initial state may have changed
+        fireEvent.click(getByTestId("CheckboxAllYear"));
+        expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);  
+    })
+});
+
+describe("tests error handling", () => {
+    it('making an incorrect selection makes an error message appear', () => {
+        const { getByTestId, container } = render(<>
+            <Provider store={store}>
+                <TimeCheckBoxGroup reportError={() => {}}/>
+            </Provider>
+            </>);
+        const errorMessage = "No month was selected. Data can only be fetched if at least one month is selected.";
     
-    expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]); // assert: if it fails the initial state may have changed
-    fireEvent.click(getByTestId("CheckboxMonth2"));
-    expect(store.getState().plot.generalSettings.months).toEqual([1, 12]);
-    fireEvent.click(getByTestId("CheckboxMonth2"));
-    expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]);
+        expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]); // assert: if it fails the initial state may have changed
+        fireEvent.click(getByTestId("CheckboxSeasonNum0"));
+        expect(container).toHaveTextContent(errorMessage);
     
-})
-
-
-it('selects a season correctly', () => {
+        fireEvent.click(getByTestId("CheckboxMonth4")); // select
+        expect(container).not.toHaveTextContent(errorMessage);
+        fireEvent.click(getByTestId("CheckboxMonth4")); // deselect
+        expect(container).toHaveTextContent(errorMessage);
     
-    const { getByTestId } = render(<>
-        <Provider store={store}>
-            <TimeCheckBoxGroup reportError={() => {}} />
-        </Provider>
-        </>);
-    
-    expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]); // assert: if it fails the initial state may have changed
-})
-
-it('selects the "All Year" checkbox correctly', () => {
-    
-    const { getByTestId } = render(<>
-        <Provider store={store}>
-            <TimeCheckBoxGroup reportError={() => {}}/>
-        </Provider>
-        </>);
-    
-    expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]); // assert: if it fails the initial state may have changed
-    fireEvent.click(getByTestId("CheckboxAllYear"));
-    expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);  
-})
-
-it('making an incorrect selection makes an error message appear', () => {
-    const { getByTestId, container } = render(<>
-        <Provider store={store}>
-            <TimeCheckBoxGroup reportError={() => {}}/>
-        </Provider>
-        </>);
-    const errorMessage = "No month was selected. Data can only be fetched if at least one month is selected.";
-
-    expect(store.getState().plot.generalSettings.months).toEqual([1, 2, 12]); // assert: if it fails the initial state may have changed
-    fireEvent.click(getByTestId("CheckboxSeasonNum0"));
-    expect(container).toHaveTextContent(errorMessage);
-
-    fireEvent.click(getByTestId("CheckboxMonth4")); // select
-    expect(container).not.toHaveTextContent(errorMessage);
-    fireEvent.click(getByTestId("CheckboxMonth4")); // deselect
-    expect(container).toHaveTextContent(errorMessage);
-
-    fireEvent.click(getByTestId("CheckboxSeasonNum3"));
-    expect(store.getState().plot.generalSettings.months).toEqual([9, 10, 11]);
-
-
-  
+        fireEvent.click(getByTestId("CheckboxSeasonNum3"));
+        expect(store.getState().plot.generalSettings.months).toEqual([9, 10, 11]);
+    });
 });
