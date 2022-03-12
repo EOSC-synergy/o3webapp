89384782cb289ca50f4efed17c228109d1359a4b
simon.vonroenn <simon.vonroenn@web.de>
2022-03-12 10:38:44 +0100

    Solve issues from last commits and switch to base 36 to shorten the string even more

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ import {
     setTitle
 } from "./store/plotSlice/plotSlice";
 import {setModel, setVisibility, setYear} from "./store/referenceSlice/referenceSlice";
-import bigInt from "big-integer";
 
 
 function parseBigInt(str, baseFrom, baseTo) {
@@ -27,29 +26,15 @@ function parseBigInt(str, baseFrom, baseTo) {
 
     baseFrom = bigInt(baseFrom);
     baseTo = bigInt(baseTo);
-    console.log(str);
     let bigint = bigInt(0);
     for (let char of str) {
-        bigint = bigInt(baseFrom * bigint + bigInt(parseInt(char, baseFrom)));
+        bigint = bigInt(bigInt(bigInt(baseFrom).multiply(bigint)).add(parseInt(char, baseFrom)));
     }
-    console.log(bigint.toString());
     let result = "";
     while (bigInt(bigint).greater(bigInt(0))) {
-        console.log(bigint);
         result = bigInt(bigInt(bigint).mod(bigInt(baseTo))).toString(baseTo).toUpperCase() + result;
         bigint = bigInt(bigInt(bigint).divide(bigInt(baseTo)));
-        console.log(bigInt(bigint).divide(baseTo).multiply(baseTo));
     }
-
-    /*
-    for (let i = 0; i < str.length; i++) {
-        let code = str[str.length-1-i].charCodeAt(0) - 48;
-        if(code >= 10) code -= 39;
-        bigint += base**bigInt(i) * bigInt(code);
-    }
-     */
-    console.log(result);
-    //return bigint.toString(base);
     return result;
 }
 
@@ -90,8 +75,7 @@ export function updateURL() {
             modelSettings.push(+modelGroup.models[model].median);
             modelSettings.push(+modelGroup.models[model].percentile);
         }
-        console.log(modelSettings);
-        modelSettings = parseBigInt(modelSettings.join(""), 2, 16);
+        modelSettings = parseBigInt(modelSettings.join(""), 2, 36);
         otherSettings.push(`group${i}="${name}",${visibilities.join("")},${models.join(",")},${modelSettings}`);
     }
 
@@ -165,7 +149,7 @@ function updateStoreWithURL() {
                 return store.getState().api.models.data[parseInt(e)];
             });
             let modelSettings = elem.split('"')[2].split(',').slice(-1)[0];
-            const binary = parseBigInt(modelSettings, 16, 2);
+            const binary = parseBigInt(modelSettings, 36, 2);
             let leadingZeros = "0" * (models.length * dataPerModel - binary.length);
             if (typeof leadingZeros === 'number') leadingZeros = "";
             modelSettings = leadingZeros + binary;
