9bc961bd80904c7dc4d446543281c79211768a3f
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-03-16 22:00:05 +0100

    updated comment

--- src/config/setupJest.js
+++ src/config/setupJest.js
@@ -1,3 +1,6 @@
+// adding code to this file will ensure that jest
+// executes it before each new unit test
+
 // This allows to add a custom handler for left-over
 // unhandled promises. Default is empty. 
 process.on('unhandledRejection', () => {});
