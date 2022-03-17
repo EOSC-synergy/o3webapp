95e100de387a831f8e6c6261998b88ebf32f2abe
Thomas Marwitz <thomasmarwitz3@gmail.com>
2022-03-17 11:45:58 +0100

    fixed import

--- src/components/Navbar/NavBar.test.js
+++ src/components/Navbar/NavBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './Navbar';
+import Navbar from './NavBar';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
